db4065faf6117f28979538786b18f5df280b6149
Angela Alvarez <angelamariaalag@gmail.com>
2025-05-28 23:27:29 -0500

    Update 3_🎚_Control_por_Sliders.py

--- pages/3_🎚_Control_por_Sliders.py
+++ pages/3_🎚_Control_por_Sliders.py
@@ -12,7 +12,7 @@ def enviar_valor_bar_graph(valor):
         st.error(f"❌ Error: {str(e)}")
 
 st.set_page_config(page_title="Control LED Bar Graph", layout="centered")
-st.title("Control de Temperatura 🔥❄️")
+st.title("Control de Luz de Proyector📽️")
 
 page_style = """
 <style>
